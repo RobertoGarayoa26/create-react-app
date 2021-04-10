bf28b2cc0718c0dba2a6f5ad16b301efbe5561b5
Roberto Garayoa <roberto_garayoaf@outlook.es>
2021-04-09 21:09:19 -0500

    Cree mi primer proyecto usando create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import User from "./User";
+
+const App = () => {
+
+    return(
+        <>
+            <h1>Hola Mundo</h1>
+            <User name="Roberto"/>
+            <User name="Alessandro"/>
+        </>
+    )
+}
+
+export default App
---
+++ src/User.jsx
@@ -0,0 +1,7 @@
+const User = ({name}) => {
+    return(
+        <div>Hola {name}</div>
+    )
+}
+
+export default User
